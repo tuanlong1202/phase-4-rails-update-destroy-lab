9b1dc7c59c82f79ce2bd009e41b585b8237a7a35
tuanlong1202 <tuanlong1202@gmail.com>
2021-09-02 23:00:23 -0400

    Phase 4 rails update destroy lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,15 +1,10 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
-  end
-
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
   end
 
   # POST /plants
@@ -18,9 +13,37 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # GET /plants/:id
+  def show
+    plant = find_plant
+    render json: plant
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
